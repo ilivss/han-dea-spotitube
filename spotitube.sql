dfc594d4ee5b210abaee5929501e9556ced71a31
ilivss <ilivss@pm.mex>
2021-03-16 08:08:08 +0100

    changes

---
+++ spotitube.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS spotitube_db;
+CREATE DATABASE spotitube_db;
+
+USE spotitube_db;
+
+DROP TABLE IF EXISTS user;
+CREATE TABLE user
+(
+  id INT NOT NULL auto_increment PRIMARY KEY,
+  username VARCHAR(50) NOT NULL UNIQUE,
+  password VARCHAR(50) NOT NULL
+);
+
+INSERT INTO user (username, password) VALUES ('john', 'doe');
+

ca27db15d4970666098b8e2285620408ddd4f342
ilivss <ilivss@pm.mex>
2021-03-18 01:26:07 +0100

    added authResource and authService tests

--- spotitube/src/test/java/han/oose/dea/resources/AuthResourceTest.java
+++ spotitube/src/test/java/han/oose/dea/resources/AuthResourceTest.java
@@ -1,29 +1,81 @@
 package han.oose.dea.resources;
 
+
+import han.oose.dea.domain.Token;
+import han.oose.dea.domain.User;
+import han.oose.dea.dto.TokenDTO;
+import han.oose.dea.dto.UserDTO;
+import han.oose.dea.exceptions.AuthException;
+import han.oose.dea.exceptions.PersistenceException;
+import han.oose.dea.mappers.TokenMapper;
+import han.oose.dea.mappers.UserMapper;
+import han.oose.dea.services.AuthService;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 
+import javax.ws.rs.core.Response;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.openMocks;
+
+//@ExtendWith(MockitoExtension.class)
+//@RunWith(MockitoJUnitRunner.class)
 public class AuthResourceTest {
-    @Test
-    public void loginSuccess() {
-        // Arrange
+    private User user;
+    private UserDTO userDTO;
+    private Token token;
+    private TokenDTO tokenDTO;
 
+    @Mock
+    private AuthService authService;
 
-        // Act
+    @Mock
+    private UserMapper userMapper;
 
+    @Mock
+    private TokenMapper tokenMapper;
 
-        // Assert
+    @InjectMocks
+    private AuthResource authResource;
 
+    @BeforeEach
+    public void setup() {
+        this.authResource = new AuthResource();
+        openMocks(this);
+
+        this.user = new User();
+        this.userDTO = new UserDTO();
+        this.token = new Token();
+        this.tokenDTO = new TokenDTO();
+
+        when(userMapper.fromDTO(userDTO)).thenReturn(user);
+        when(tokenMapper.toDTO(token)).thenReturn(tokenDTO);
     }
 
     @Test
-    public void loginFail() {
+    public void loginSuccess() throws PersistenceException, AuthException {
         // Arrange
-
+        when(authService.login(user)).thenReturn(token);
 
         // Act
-
+        Response response = this.authResource.login(userDTO);
 
         // Assert
+        assertEquals(Response.Status.OK, response.getStatusInfo());
+        assertEquals(tokenDTO, response.getEntity());
+    }
 
+    @Test
+    public void loginFailure() throws PersistenceException {
+        // Arrange
+        when(authService.login(user)).thenReturn(null);
+
+        // Act & Assert
+        assertThrows(AuthException.class, () -> authResource.login(userDTO));
     }
 }
--- spotitube/src/test/java/han/oose/dea/services/AuthServiceTest.java
+++ spotitube/src/test/java/han/oose/dea/services/AuthServiceTest.java
@@ -1,18 +1,64 @@
 package han.oose.dea.services;
 
+import han.oose.dea.dao.ITokenDAO;
+import han.oose.dea.dao.IUserDAO;
+import han.oose.dea.domain.Token;
+import han.oose.dea.domain.User;
+import han.oose.dea.exceptions.PersistenceException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.openMocks;
 
 class AuthServiceTest {
-    @Test
-    public void loginSuccess() {
-        // Arrange
+    private User user;
 
+    @Mock
+    private IUserDAO userDAO;
 
-        // Act
+    @Mock
+    private ITokenDAO tokenDAO;
 
+    @InjectMocks
+    private AuthService authService;
 
-        // Assert
+    @BeforeEach
+    public void setup() {
+        this.authService = new AuthService();
+        openMocks(this);
 
+        user = new User();
     }
 
+    @Test
+    public void loginSuccess() throws PersistenceException {
+        // Arrange
+        Token expectedToken = new Token();
+        when(userDAO.getUser(user)).thenReturn(user);
+        when(tokenDAO.generateAndSaveToken(any())).thenReturn(expectedToken);
+
+        // Act
+        Token actualToken = authService.login(user);
+
+        // Assert
+        assertEquals(expectedToken, actualToken);
+    }
+
+    @Test
+    public void loginFailure() throws PersistenceException {
+        // Arrange
+        when(userDAO.getUser(user)).thenReturn(null);
+
+        // Act
+        Token actualToken = authService.login(user);
+
+        // Assert
+        assertNull(actualToken);
+    }
 }

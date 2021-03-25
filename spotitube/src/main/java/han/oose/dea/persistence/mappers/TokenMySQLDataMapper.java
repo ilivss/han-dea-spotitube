bed9b3a8a003ad348a65285e52517806e8c8db12
ilivss <ilivss@pm.mex>
2021-03-25 16:34:35 +0100

    added Token mysql mapper

---
+++ spotitube/src/main/java/han/oose/dea/persistence/mappers/TokenMySQLDataMapper.java
@@ -0,0 +1,24 @@
+package han.oose.dea.persistence.mappers;
+
+import han.oose.dea.domain.Token;
+
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class TokenMySQLDataMapper implements IDataMapper <ResultSet, Token> {
+    @Override
+    public Token toDomainObject(ResultSet resultSet) {
+        try {
+            return new Token(
+                    resultSet.getString("username"),
+                    resultSet.getString("token"),
+                    resultSet.getDate("expiry_date")
+                    );
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
